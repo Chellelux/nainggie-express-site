f87a8e8854991a765ceedc9bdde50ef96a8f6b70
Ritchelle <mrnainggie@tip.edu.ph>
2026-02-13 03:15:32 +0800

    Final fix: Use server.js and add fs module

--- server.js
+++ server.js
@@ -18,6 +18,21 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
+app.get('/api/posts', (req, res) => {
+    const dataPath = path.join(__dirname, 'data', 'posts.json');
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send("Error reading blog data.");
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+// Route to serve the Blog HTML page
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
